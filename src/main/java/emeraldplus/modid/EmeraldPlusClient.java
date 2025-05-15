b5639d150b4b73266b43ea785f407cd0c7e79bf9
Peter Tso <petertso@Peters-Laptop.local>
2025-05-15 15:15:35 -0700

    added pickaxe

---
+++ src/main/java/emeraldplus/modid/EmeraldPlusClient.java
@@ -0,0 +1,10 @@
+package emeraldplus.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EmeraldPlusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/emeraldplus/modid/item/ModItems.java
@@ -0,0 +1,23 @@
+package emeraldplus.modid.item;
+
+import emeraldplus.modid.EmeraldPlus;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item NETHER_EMERALD = registerItem("nether_emerald", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(EmeraldPlus.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        EmeraldPlus.LOGGER.info("Registering Mod Items for " + EmeraldPlus.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {entries.add(NETHER_EMERALD);});
+    }
+}
